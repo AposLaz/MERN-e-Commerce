69e2bc2ac30b7dfe189b2893f703bd839414a8b1
AposLaz <lazidisa@otenet.gr>
2022-09-30 17:28:56 +0300

    Add frontend & backend folders

---
+++ frontend/README.md
@@ -0,0 +1,4 @@
+# Getting Started with E-Commerce
+
+This is a e-commerce website that can used for production jobs. 
+
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">{process.env.REACT_APP_LOGO_NAME}</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
